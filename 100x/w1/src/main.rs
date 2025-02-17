521988e7281a4476a32c7631bccea3b443e35302
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-02-17 19:00:46 +0530

    i-dtype

---
+++ 100x/w1/src/main.rs
@@ -0,0 +1,17 @@
+// Similar to typescript
+fn main() {
+    // let res: u32 = sum(5,4);    
+    // println!("{}" , res);
+
+    // let name = "Jayesh";
+    // let name : &str = "Jayesh";
+    let name : String = String::from("Jayesh");
+    println!("{}",name);
+
+    // Vectos
+    let v = vec![1,2,3,4];
+    println!("{:?}",v);
+}
+// fn sum(a:u32, b:u32) -> u32 {
+//     return a + b;
+// }
